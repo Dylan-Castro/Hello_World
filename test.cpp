903b42ffb16ff58baf424d5169c186181d9fd5b3
Dylan <dylan.castro@utec.edu.pe>
2019-03-28 12:32:00 -0500

    correccion

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
   string texto;
   cin >> texto;
-  map<string,int> mapa;
+  map<char,int> mapa;
   for(int x=0;x<texto.size();x++){
     if(mapa.count(texto[x])!=0){
       mapa[texto[x]]=mapa[texto[x]]+1;
@@ -14,6 +14,6 @@ int main(){
       mapa[texto[x]]=1;
     }
   }
-
-
+  for (std::map<char,int>::iterator it=mapa.begin(); it!=mapa.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
 }
